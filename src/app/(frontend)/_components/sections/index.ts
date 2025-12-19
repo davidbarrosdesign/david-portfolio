b3ca838919f2548d002ff922980e90dcac8eb8a1
David Barros <davidbarrosdesign@gmail.com>
2025-12-19 00:33:51 -0300

    fix: exclude section home to index

--- src/app/(frontend)/_components/sections/index.ts
+++ src/app/(frontend)/_components/sections/index.ts
@@ -1,9 +1,8 @@
 import { Header } from "./header/Header";
 import { Footer } from "./footer/Footer";
 import { HeroHome } from "./hero-home/HeroHome";
-import { ClientsHome } from "./clients-home/ClientsHome";
 import { CallToAction } from "./call-to-action/CallToAction";
 import { HeroPage } from "./hero-page/HeroPage";
 import { CookieBanner } from "./cookie-banner/CookieBanner";
 
-export { HeroHome, ClientsHome, Header, Footer, CallToAction, HeroPage, CookieBanner };
+export { HeroHome, Header, Footer, CallToAction, HeroPage, CookieBanner };
