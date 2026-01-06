172cb06dda9db860606b011222ebcb8ed54c9e22
David Barros <davidbarrosdesign@gmail.com>
2026-01-06 13:55:30 -0300

    fix: ajustment in user status

--- src/collections/Testimonials.ts
+++ src/collections/Testimonials.ts
@@ -11,7 +11,7 @@ export const Testimonials: CollectionConfig = {
   },
   access: {
     read: ({ req: { user } }) => {
-      if (user) return true
+      if (user) return false
       
       return {
         _status: {
